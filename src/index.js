0152781a6d900eb32e24376c936a18d5304ced4e
RybalkoYakov <RybalkoYakovv@gmail.com>
2023-01-10 21:41:12 +0300

    feat: 100 test pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function toReadable (number) {
-  
+    const NUMBERS_0 = [
+        'zero',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen'
+    ]
+
+    const NUMBERS_1 = [
+        'twenty',
+        'thirty',
+        'forty',
+        'fifty',
+        'sixty',
+        'seventy',
+        'eighty',
+        'ninety'
+    ]
+
+    if (number >= 0 && number <= 19) {
+        return  NUMBERS_0[number];
+    } else if (number >= 20 && number <= 99) {
+        const dozens = parseInt(number.toString()[0])
+        const units = parseInt(number.toString()[1])
+        if (units === 0) {
+            return NUMBERS_1[dozens - 2]
+        } else {
+            return `${NUMBERS_1[dozens - 2]} ${NUMBERS_0[units]}`
+        }
+    } else if (number => 100 && number <= 999) {
+        const hundreds = parseInt(number.toString()[0])
+        const dozens = parseInt(number.toString()[1])
+        const units = parseInt(number.toString()[2])
+
+
+    }
 }
